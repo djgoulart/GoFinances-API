b7c59d4a55859e6e7e0b21299ac4daa6a253401f
Diego Goulart <diegogoulart.aws@gmail.com>
2020-08-21 19:37:18 -0300

    add procfile

---
+++ dist/ormconfig.js
@@ -0,0 +1,10 @@
+"use strict";
+module.exports = {
+    "type": postgres,
+    "url": process.env.DATABASE_URL,
+    "entities": "./dist/models/*.js",
+    "migrations": "./dist/database/migrations/*.js",
+    "cli": {
+        "migrationsDir": "./src/database/migrations"
+    }
+};
